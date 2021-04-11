38bb38cbac2945e217218f70ba870bcf825161a8
amaurysoares <amauryrodrigo@gmail.com>
2021-04-11 20:21:03 -0300

    commit inicial

---
+++ application/index.php
@@ -0,0 +1,32 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+ob_start();
+session_start();
+
+require __DIR__.'/vendor/autoload.php';
+
+use CoffeeCode\Router\Router;
+$router = new Router(site());
+
+$router->group(null);
+$router->get("/", "Web:login", "web:login");
+$router->get("/recuperar", "Web:register", "web.register");
+$router->get("/recuperar", "Web:forget", "web.forget");
+$router->get("/senha", "Web:password", "web.password");
+
+
+
+$router->dispatch();
+
+
+if($router->error()){
+    $router->redirect("web.error", ["errcode" => $router->error()]);
+}
+
+
+
+ob_end_flush();
