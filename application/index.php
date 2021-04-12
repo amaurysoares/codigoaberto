b7bdb63309b9506c32575e874218d2339ef40d5c
amaurysoares <amauryrodrigo@gmail.com>
2021-04-12 19:58:26 -0300

    adicionados controllers

--- application/index.php
+++ application/index.php
@@ -12,12 +12,13 @@ require __DIR__.'/vendor/autoload.php';
 use CoffeeCode\Router\Router;
 $router = new Router(site());
 
+$router->namespace(("Source\Controllers"));
+
 $router->group(null);
-$router->get("/", "Web:login", "web:login");
-$router->get("/recuperar", "Web:register", "web.register");
+$router->get("/", "Web:login", "web.login");
+$router->get("/register", "Web:register", "web.register");
 $router->get("/recuperar", "Web:forget", "web.forget");
 $router->get("/senha", "Web:password", "web.password");
-
 
 
 $router->dispatch();
